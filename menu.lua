f21473c8cb01bc4155e8df6a8bfc66ae6a827ee5
das-jasper <jasper.schwarzwald@gmx.de>
2018-08-25 21:42:18 +0200

    - added menu_init menu_update and menu_draw to main.lua

--- menu.lua
+++ menu.lua
@@ -6,5 +6,14 @@ local function init_menu()
 end
 
 function update_menu(dt)
-	
+	if love.keyboard.isDown("up") then
+		current_menu_state = current_menu_state + 1
+	elseif love.keyboard.isDown("down") then
+		current_menu_state = current_menu_state - 1
+	if current_menu_state < 0 then
+		current_menu_state = 0
+end
+
+function draw_menu()
+
 end
