b4e4e4482ce7d5551c304a9a1c6fa421e4899b0f
das-jasper <jasper.schwarzwald@gmx.de>
2018-08-25 22:38:43 +0200

    - added menu.keyPressed

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
+local menu = require "menu"
 local pos
 
 function love.load(arg)
+    menu.init_menu()
+
     pos = 100
     love.window.setMode(600, 400, {resizable=true, vsync=false, minwidth=400, minheight=300, msaa=4})
 end
 
 function love.update(dt)
+    menu.update_menu(dt)
+
     if (love.keyboard.isDown("right")) then
         pos = pos + dt * 500
     end
@@ -15,6 +20,12 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.circle("fill", pos, 100, 50, 64)
-    love.graphics.print("Hello World", 400, 300)
+    menu.draw_menu()
+end
+
+function love.keypressed(key)
+    menu.keyPressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
--- menu.lua
+++ menu.lua
@@ -1,19 +1,39 @@
 local current_menu_state
+local current_menu_state_string
 
-local function init_menu() 
+local menu = {}
+
+function menu.init_menu() 
 	print("initialized menu")
 	current_menu_state = 0
+    current_menu_state_string = "test0"
 end
 
-function update_menu(dt)
-	if love.keyboard.isDown("up") then
-		current_menu_state = current_menu_state + 1
-	elseif love.keyboard.isDown("down") then
-		current_menu_state = current_menu_state - 1
-	if current_menu_state < 0 then
-		current_menu_state = 0
+function menu.update_menu(dt)
+    if current_menu_state == 0 then
+        current_menu_state_string = "test0"
+    elseif current_menu_state == 1 then
+        current_menu_state_string = "test1"
+    end
+    if current_menu_state < 0 then
+        current_menu_state = 0
+    elseif current_menu_state > 1 then
+        current_menu_state = 1
+    end
 end
 
-function draw_menu()
+function menu.draw_menu()
+    love.graphics.setColor(1, 1, 1)
 
+    love.graphics.print(current_menu_state_string, 300, 200)
 end
+
+function menu.keyPressed(key)
+    if key == "down" then
+        current_menu_state = current_menu_state - 1
+    elseif key == "up" then
+        current_menu_state = current_menu_state + 1
+    end
+end
+
+return menu
